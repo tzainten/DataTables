bd2e7f488263aecbd06d245ddd8798ade2264028
tzainten <122052876+tzainten@users.noreply.github.com>
2024-12-17 17:19:26 -0500

    First (totally garbage) implementation of TableView

--- Editor/DataTableEditor.cs
+++ Editor/DataTableEditor.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.Json;
 using DataTables;
+using DataTablesEditor.Widgets;
 using Editor;
 using Sandbox;
 using Json = DataTables.Json;
@@ -16,10 +18,17 @@ public class DataTableEditor : DockWindow
 	private Asset _asset;
 	private DataTable _dataTable;
 
+	private ToolBar _toolBar;
+
+	private List<RowStruct> _internalEntries;
+
+	private TableView _tableView;
+
 	public DataTableEditor( Asset asset, DataTable dataTable )
 	{
 		_asset = asset;
 		_dataTable = dataTable;
+		_internalEntries = _dataTable.StructEntries;
 
 		DeleteOnClose = true;
 
@@ -27,18 +36,119 @@ public class DataTableEditor : DockWindow
 		Title = "Data Table Editor";
 		SetWindowIcon( "equalizer" );
 
+		AddToolbar();
+
+		Canvas = new();
+		Canvas.Layout = Layout.Column();
+		var layout = Canvas.Layout;
+
+		_tableView = new TableView( Canvas );
+		_tableView.MinimumHeight = 200;
+
+		var structType = TypeLibrary.GetType( _dataTable.StructType );
+
+		var rowNameCol = _tableView.AddColumn();
+		rowNameCol.Name = "RowName";
+		rowNameCol.Value = o =>
+		{
+			return structType.GetProperty( "RowName" ).GetValue( o )?.ToString() ?? "";
+		};
+
+		int i = 0;
+		foreach ( var property in structType.Properties.Where( x => x.IsPublic && !x.IsStatic ) )
+		{
+			if ( property.Name == "RowName" )
+				continue;
+
+			int idx = i;
+			var col = _tableView.AddColumn();
+			col.Name = property.Name;
+			col.Value = o =>
+			{
+				return property.GetValue( o )?.ToString() ?? "";
+			};
+			i++;
+		}
+
+		_tableView.SetItems( _internalEntries.ToList() );
+		_tableView.FillHeader();
+
+		Splitter splitter = new(this);
+		splitter.IsVertical = true;
+
+		var sheetCanvas = new Widget();
+		sheetCanvas.Layout = Layout.Column();
+		sheetCanvas.MinimumHeight = 300;
+
+		var sheet = new ControlSheet();
+		sheetCanvas.Layout.Add( sheet );
+		sheetCanvas.Layout.AddStretchCell();
+		sheetCanvas.OnPaintOverride = () =>
+		{
+			Paint.ClearPen();
+			Paint.SetBrush( Theme.WidgetBackground );
+			Paint.DrawRect( sheetCanvas.LocalRect );
+
+			return false;
+		};
+
+		_tableView.ItemClicked = o =>
+		{
+			sheet.Clear( true );
+			sheet.AddObject( o.GetSerialized() );
+		};
+
+		splitter.AddWidget( _tableView );
+		splitter.AddWidget( sheetCanvas );
+		splitter.SetCollapsible( 0, false );
+		splitter.SetCollapsible( 1, false );
+
+		layout.Add( splitter );
+
 		Show();
+	}
+
+	private void AddToolbar()
+	{
+		_toolBar = new ToolBar( this, "DataTableToolbar" );
+		AddToolBar( _toolBar, ToolbarPosition.Top );
+
+		_toolBar.Movable = false;
+		_toolBar.AddOption( "Save", "common/save.png" ).StatusTip = "Saves this Data Table to disk";
+		_toolBar.AddOption( "Browse", "common/browse.png" ).StatusTip = "Filler";
+		_toolBar.AddSeparator();
+		_toolBar.AddOption( "Add", "common/add.png", AddEntry ).StatusTip = "Append a new entry";
+		_toolBar.AddOption( "Duplicate", "common/copy.png" ).StatusTip = "Appends a duplicate of the currently selected entry";
+		_toolBar.AddOption( "Delete", "common/remove.png" ).StatusTip = "Delete the currently selected entry";
+
+		var stretch = new Widget();
+		stretch.HorizontalSizeMode = SizeMode.CanGrow | SizeMode.Expand;
+		_toolBar.AddWidget( stretch );
+
+		var dropdown = new Dropdown( _dataTable.StructType );
+		dropdown.FixedWidth = 200;
+		dropdown.Icon = "account_tree";
+		_toolBar.AddWidget( dropdown );
+	}
+
+	private void AddEntry()
+	{
+		var o = TypeLibrary.Create<RowStruct>( _dataTable.StructType );
+		_internalEntries.Add( o );
+		_tableView.AddItem( o );
 	}
 
 	[Shortcut( "editor.save", "CTRL+S", ShortcutType.Window )]
 	private void Save()
 	{
+		_dataTable.StructEntries = _internalEntries;
 		var json = Json.Serialize( _dataTable, writer =>
 		{
 			writer.WritePropertyName( "__references" );
 
 			var references = _asset.GetReferences( false );
 			Json.SerializeArray( writer, references.Select( x => x.Path ) );
+
 			writer.WritePropertyName( "__version" );
 			writer.WriteNumberValue( _dataTable.ResourceVersion );
 		} );
---
+++ Editor/Widgets/Table.cs
@@ -0,0 +1,242 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Editor;
+using Sandbox;
+using Sandbox.UI;
+using Sandbox.VR;
+using Label = Editor.Label;
+
+namespace DataTablesEditor.Widgets;
+
+/// <summary>
+/// I am absolutely not proud of this at all.
+/// </summary>
+internal class TableView : Widget
+{
+	public class Column
+	{
+		private string _name;
+		public string Name
+		{
+			get
+			{
+				return _name;
+			}
+			set
+			{
+				if ( value is null )
+					_name = null;
+				else
+				{
+					StringBuilder result = new StringBuilder();
+					result.Append( value[0] );
+
+					for (int i = 1; i < value.Length; i++)
+					{
+						if ( char.IsUpper( value[i] ) && !char.IsUpper( value[i - 1] ) )
+							result.Append( ' ' );
+
+						result.Append( value[i] );
+					}
+
+					_name = result.ToString();
+				}
+			}
+		}
+		public int Width;
+		public Func<object, string> Value;
+		public TextFlag TextFlag = TextFlag.Left;
+		public Label Label;
+	}
+
+	public ListView ListView { get; set; }
+
+	public List<Column> Columns { get; set; } = new();
+
+	TableHeader Header;
+
+	public Action<object> ItemClicked;
+
+	public Dictionary<VirtualWidget, int> WidgetIndexMap = new();
+
+	public TableView( Widget parent ) : base( parent )
+	{
+		ListView = new( this );
+
+		ListView.ItemPaint = widget => PaintRow( widget );
+		ListView.ItemSize = new Vector2( 0, 24 );
+		ListView.ItemSpacing = 0;
+		ListView.ItemClicked = o =>
+		{
+			if ( ItemClicked is not null )
+				ItemClicked( o );
+		};
+		ListView.Margin = new Margin( 0, 4, 0, 0 );
+
+		Layout = Layout.Column();
+		Layout.Add( Header = new TableHeader( this ) );
+		Layout.Add( ListView );
+	}
+
+	public Column AddColumn( string name = null, int? width = null, Func<object, string> value = null )
+	{
+		var col = new Column();
+		col.Name = name;
+		col.Width = width ?? 50;
+		col.Value = value;
+
+		Columns.Add( col );
+
+		return col;
+	}
+
+	protected override void DoLayout()
+	{
+		base.DoLayout();
+
+		int i = 0;
+		foreach ( var lbl in Header.Labels )
+		{
+			Columns[i].Width = (int)lbl.Width;
+			i++;
+		}
+	}
+
+	public void FillHeader()
+	{
+		foreach ( var column in Columns )
+		{
+			Header.AddColumn( column );
+		}
+	}
+
+	public void SetItems<T>( IEnumerable<T> items )
+	{
+		foreach ( var elem in items.ToList().Cast<object>() )
+		{
+			ListView.AddItem( elem );
+		}
+	}
+
+	public void AddItem( object item )
+	{
+		ListView.AddItem( item );
+	}
+
+	private void PaintRow( VirtualWidget widget )
+	{
+		/*if ( widget.Object is not T t )
+			return;*/
+
+		var isAlt = widget.Row % 2 == 0;
+		var backgroundColor = widget.Selected ? Theme.Selection : ( widget.Hovered ? Theme.ButtonDefault.WithAlpha( 0.7f ) : Theme.ButtonDefault.WithAlpha( isAlt ? 0.3f : 0.2f ) );
+
+		Paint.ClearPen();
+		Paint.SetBrush( backgroundColor );
+		Paint.DrawRect( widget.Rect );
+
+		Rect rect = widget.Rect;
+
+		Paint.SetDefaultFont();
+		Paint.SetPen( widget.Selected ? Color.Orange : Theme.ControlText );
+
+		foreach ( var column in Columns )
+		{
+			var width = column.Width + 4;
+			rect.Width = width;
+			Paint.DrawText( rect.Shrink( 8, 0 ), column.Value(widget.Object), column.TextFlag | TextFlag.CenterVertically | TextFlag.SingleLine );
+			rect.Left += width;
+		}
+	}
+
+	class TableHeader : Widget
+	{
+		readonly TableView Table;
+
+		public List<Label> Labels = new();
+
+		public HeaderSplitter _splitter;
+
+		public static TableHeader Header = null;
+
+		[EditorEvent.Frame]
+		public static void Frame()
+		{
+			if ( Header is null || !Header.IsValid )
+				return;
+
+			var list = Header._splitter.Labels;
+			for ( int i = 0; i < list.Count; i++ )
+			{
+				var lbl = list[i];
+				Header.Table.Columns[i].Width = (int)lbl.Width;
+
+				var _ = new Widget();
+				Header.Table.ListView.AddItem( _ );
+				Header.Table.ListView.RemoveItem( _ );
+			}
+		}
+
+		public TableHeader( TableView parent ) : base( parent )
+		{
+			Table = parent;
+			MinimumHeight = 25;
+
+			Layout = Layout.Row();
+
+			_splitter = new(this);
+			_splitter.IsHorizontal = true;
+
+			Header = this;
+
+			Layout.Add( _splitter );
+		}
+
+		public override void Close()
+		{
+			base.Close();
+
+			Header = null;
+		}
+
+		private int splitterCount = 0;
+		public void AddColumn( Column column )
+		{
+			Labels.Add( _splitter.AddColumn( column ) );
+		}
+	}
+
+	public class HeaderSplitter : Splitter
+	{
+		public List<Label> Labels = new();
+
+		public HeaderSplitter(Widget parent) : base(parent)
+		{
+		}
+
+		private int splitterCount = 0;
+		public Label AddColumn( Column column )
+		{
+			var lbl = new Label( column.Name );
+			lbl.ContentMargins = new Margin( 8, 0, 0, 0 );
+			lbl.OnPaintOverride = () =>
+			{
+				Paint.SetBrush( ControlWidget.ControlColor );
+				Paint.SetPen( Color.Transparent );
+				Paint.DrawRect( lbl.LocalRect );
+
+				return false;
+			};
+			column.Label = lbl;
+			Labels.Add( lbl );
+
+			AddWidget( lbl );
+			SetCollapsible( splitterCount++, false );
+
+			return lbl;
+		}
+	}
+}
